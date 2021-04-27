66dbd2089b86c1da7807ad45a60d70336403688d
Erik Eklund <erik@eklundsolutions.se>
2021-04-27 11:12:13 +0200

    add driver

--- src/main/java/com/erik/driver/DriverController.java
+++ src/main/java/com/erik/driver/DriverController.java
@@ -1,12 +1,17 @@
 package com.erik.driver;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class DriverController {
@@ -21,17 +26,33 @@ public class DriverController {
         return "Driver API för JAd-21 (Erik Eklund)";
     }
 
-
+    // Hämta alla drivers
     @GetMapping(path="/driver")
     @CrossOrigin()
     List<Driver> getAll(){
         return driverService.getAll();
     }
 
+    // Hämta angiven driver
     @GetMapping(path="/driver/{id}")
     @CrossOrigin()
     Driver getSingle(@PathVariable Integer id){
         return driverService.get(id);
+    }
+
+
+    
+    // Skapa spelare (POST). Förväntar oss JSON (definierad som)
+    @PostMapping(path = "/add-driver", consumes="application/json", produces="application/json")
+    @CrossOrigin()
+    ResponseEntity<Object> add(@RequestBody Driver d) {
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(d.getId())
+        .toUri();
+        return ResponseEntity.created(location).build();
+
     }
 
     
